8f7cf03d3b7c3782947883a88f33910017b30db8
LuisJongInd <luisjongind@gmail.com>
2023-03-19 21:55:51 -0600

    .

--- omni_robots/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/luis/catkin_workspaces/lab_casci/omni_robots/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- omni_robots/build/communication_nodes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/catkin_workspaces/lab_casci/omni_robots/src/communication_nodes
-# Build directory: /home/luis/catkin_workspaces/lab_casci/omni_robots/build/communication_nodes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
